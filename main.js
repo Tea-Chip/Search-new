d02832010c2b5e642d201c44eab42f5ffb02782b
TomiPerfect <52126478+Tobiloba08@users.noreply.github.com>
2020-05-27 14:32:44 +0100

    2nd update

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-Swal.fire({
-  title: '<strong>HTML <u>example</u></strong>',
-  icon: 'info',
-  html:
-    'You can use <b>bold text</b>, ' +
-    '<a href="//sweetalert2.github.io">links</a> ' +
-    'and other HTML tags',
-  showCloseButton: true,
-  showCancelButton: true,
-  focusConfirm: false,
-  confirmButtonText:
-    '<i class="fa fa-thumbs-up"></i> Great!',
-  confirmButtonAriaLabel: 'Thumbs up, great!',
-  cancelButtonText:
-    '<i class="fa fa-thumbs-down"></i>',
-  cancelButtonAriaLabel: 'Thumbs down'
-})
+ alert("Get In Touch");
+ 
+ confirm("Get In Touch")
+ true
+ 
+ confirm("Get In Touch")
+ false
+ 
+ const answer = confirm("Get In Touch")
+ 
+ if (answer) {
+   console.log("user wants to proceed");
+ }
+ 
+ 
+
